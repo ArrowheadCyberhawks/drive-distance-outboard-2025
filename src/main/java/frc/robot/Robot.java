9a6f86d2c8a06a53cedaa843486e38471fd224fd
Cyberhawks706 <cyberhawks706@gmail.com>
2026-01-22 20:44:37 -0600

    Updated gradle to 2026

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.DriveSubsystem;
+// import frc.robot.subsystems.DriveSubsystem;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
